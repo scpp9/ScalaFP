d255a919f353b73180cf4b5731e3177e98c670d9
scpp9 <srikanth.chadalavada@gmail.com>
2022-01-24 21:42:33 -0600

    Day 5, 6, 7, 8

--- scalaz-nick-patridge/src/main/scala/example/Day7.scala
+++ scalaz-nick-patridge/src/main/scala/example/Day7.scala
@@ -3,13 +3,25 @@ package example
 
 class Day7 {
 
-  type stack = List[Int]
+  type Stack = List[Int]
 
-  def pop(s0: stack): (stack, Int) = s0 match {
-    case x:: xs => (xs, x)
+  import cats._, cats.data._, cats.syntax.all._
+
+  val pop = State[Stack, Int] {
+    case x :: xs => (xs, x)
     case Nil => sys.error("stack is empty")
   }
 
+  def push(a: Int) = State[Stack,Unit] {
+    case xs => (a :: xs, ())
+  }
 
+  def stackManip: State[Stack, Int] = for {
+    _ <- push(3)
+    a <- pop
+    b <- pop
+  } yield(b)
+
+  stackManip.run(List(5,8,2,1)).value
 
 }
